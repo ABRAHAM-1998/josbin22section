10fbf6d25322c4da8ae6395728aad0c6d4645b8e
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-09-27 15:34:36 +0530

    new chat bring up

--- src/app/COMPfOLDER/CHATFOLDER/chat/chat.component.ts
+++ src/app/COMPfOLDER/CHATFOLDER/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from "../../../SHARED/data.service";
+import { ApiserviceService } from 'src/app/SHARED/apiservice.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,15 +8,31 @@ import { DataService } from "../../../SHARED/data.service";
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+  
+  ngOnInit(): void {
+
+    this.fn_UserProfile();
+  }
+
+  fn_UserProfile() {
+    let data ={ id: localStorage.getItem('id') }
+
+    this.api.methPOst('userp',data).subscribe((res) => {
+      this.user = res['arrList']['username']
+    }, (error) => {
+      // console.log(error, 'this is my error')
+    })
+  }
 
   title = 'instant-chatting';
   // user: String;
-  public user = localStorage.getItem('id');
+
+  public user = ''
   room: String;
   // tslint:disable-next-line:ban-types
   messageText: String;
   messageArray: Array<{user: String , message: String }> = [];
-  constructor(private instantChatservice: DataService){
+  constructor(private instantChatservice: DataService, private api:ApiserviceService){
     this.instantChatservice.newUserJoined()
       .subscribe(data => this.messageArray.push(data));
 
@@ -29,6 +46,7 @@ export class ChatComponent {
 
   join(){
       this.instantChatservice.joinRoom({user: this.user, room: this.room});
+      console.log(this.user)
   }
 
   leave(){
@@ -37,8 +55,15 @@ export class ChatComponent {
 
   sendMessage()
   {
+    if(this.messageText != ''){
     this.instantChatservice.sendMessage({user: this.user, room: this.room, message: this.messageText});
+    this.messageText = ''}
+    else{
+      
+    }
   }
+
+
 
 
 }
